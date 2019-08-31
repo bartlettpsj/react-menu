d7edc058f436eaecaa301e04e45e3274b8f3309b
Paul Bartlett <paul.bartlett@everyonecounts.com>
2019-08-31 11:58:40 -0700

    add important files

---
+++ src/Menu.js
@@ -0,0 +1,131 @@
+import React, { Fragment } from "react";
+import styled from "react-emotion";
+import css from 'react-emotion';
+// import { Link } from "react-dom";
+import _ from 'lodash';
+
+const Backdrop = styled('div')`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  z-index: 1;
+`;
+
+const Container = styled('div')`
+  //position: relative;
+  height: 100%;
+  width: 200px;
+  background: white;
+  padding-top: 1.5em;
+  //padding: 1.5rem;
+  overflow-y: auto;
+  z-index: 2;  
+  //left: 500px;
+  //position: absolute;  
+`;
+
+const Item = styled('a')`
+  line-height: 2.0em;
+  //width: 100%;
+  display: block;  
+
+  &:hover {
+    font-weight: bold;
+  }
+  
+  &:hover + div {
+    display: block;
+  }  
+
+  
+`;
+
+const dynamicStyle = props =>
+  css`
+    color: ${props.color};
+  `;
+
+const SubContainer = styled('div')`
+  display: none;
+  position: absolute;
+  //left: 200px;
+  left: 50%;
+  top: 0;
+  //padding: 1rem;
+  background: white;
+  //max-height: 700px;
+  //width: 75%;
+  //display: flex;
+  //flex-flow: column wrap;
+  z-index: 3;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  
+  //${dynamicStyle}
+  
+`;
+
+const Menu = ({categories, onClose,  ...props }) => {
+
+  // console.log(a,b);
+
+  let key = 0;
+
+  const logEvent = (e) => {
+    console.log(`${e.type} Event client [${e.clientX},${e.clientY}] screen [${e.screenX}, ${e.screenY}]`);
+  }
+
+  const EnterHover = (e) => {
+    logEvent(e);
+
+    // Need to reposition div child based on this
+    //How do we find the child
+    // How do we change the css for that child using react
+  }
+
+  const LeaveHover = (e) => {
+    logEvent(e);
+  }
+
+
+  const MouseOver = (e) => {
+    logEvent(e);
+  }
+
+  const MouseOut = (e) => {
+    logEvent(e);
+  }
+
+  const clicky = (w,e) => {
+    logEvent(e);
+  }
+
+  const mover = (e) => {
+    logEvent(e);
+  }
+
+
+  return (
+    <Backdrop>
+      <Container>
+        {_.times(20, () => categories.map(category => (
+          <Fragment>
+            <Item key={++key} onMouseOver={MouseOver} onMouseOut={MouseOut} onMouseEnter={EnterHover} onMouseLeave={LeaveHover}>{category.id}: {category.name}</Item>
+            <SubContainer>
+            {category.children.map(sub => (
+                <Item key={++key} onMouseOver={MouseOver} onMouseOut={MouseOut} onMouseEnter={EnterHover} onMouseLeave={LeaveHover}>{sub.id}: {sub.name}</Item>
+            ))}
+            </SubContainer>
+          </Fragment>
+        )))}
+      </Container>
+    </Backdrop>
+  )
+}
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Menu from './Menu'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const doClose = () => {
+  console.log('closing');
+}
+
+const cats =
+  [
+    {
+      id: '123',
+      name: ' 1-2-3',
+      children: [
+        {
+          id: 'abc',
+          name: ' a-b-c'
+        },
+        {
+          id: 'def',
+          name: ' d-e-f'
+        }
+      ]
+    },
+    {
+      id: '456',
+      name: ' 4-5-6',
+      children: [
+        {
+          id: 'xyz',
+          name: ' x-y-z'
+        }
+      ]
+    }
+  ];
+
+const test = {
+  a: 1,
+  b: 2
+}
+
+ReactDOM.render(<Menu categories={cats} {...test} onClose={doClose}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
