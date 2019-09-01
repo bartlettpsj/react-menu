5dbe742707fcac1c60450ae4372da0f610b1e0f7
Paul Bartlett <paul.bartlett@walmartlabs.com>
2019-09-01 14:23:42 -0700

    WIP playing - refs next

--- src/Menu.js
+++ src/Menu.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import styled, { css } from "react-emotion";
 // import { Link } from "react-dom";
 import _ from 'lodash';
@@ -27,7 +27,12 @@ const Container = styled('div')`
   //position: absolute;  
 `;
 
-const Item = styled('a')`
+const MyItem = (props) => (
+  <a {...props} banana={'hello'}/>
+)
+
+// const Item = styled('a')`
+const Item = styled(MyItem)`
   line-height: 2.0em;
   //width: 100%;
   display: block;  
@@ -36,25 +41,51 @@ const Item = styled('a')`
     font-weight: bold;
   }
   
-  &:hover + div {
-    display: block;
+  &:hover > div {
+    //display: block;
+    //border: blue 4px solid;
+    visibility: visible;
   }  
-
-  
 `;
 
-const dynamicStyle = props =>
-  css`
-    color: ${props.color};
-    background-color: green;
-  `;
+const MyStyledSubItem = styled(Item)`
+  background-color: hotpink;
+  color: ${props => props.color};
+  //visibility: ${props => props.visibility};    
+`;
 
-const SubContainer = styled('div')`
-  display: none;
+const MySubItem = (props) => (
+  <MyStyledSubItem {...props}/>
+)
+
+const MyStyledStyledSubItem = styled(MyStyledSubItem)`
+  color: green;
+`
+
+const MyStyledMySubItem = styled(MySubItem)`
+  color: blanchedalmond;
+`
+
+// const dynamicStyle = props =>
+//   css`
+//     color: ${props.color};
+//     background-color: green;
+//   `;
+
+const calcLeft = (left) => {
+  return left+100 + 'px';
+}
+
+const calcTop = (top) => {
+  return '100px';
+}
+const StyledSubContainer = styled('div')`
+  visibility: ${props => props.visibility};    
+  //display: none;
   position: absolute;
-  //left: 200px;
-  left: 50%;
-  top: 0;
+  left: ${props => calcLeft(props.left)};
+  //left: 50%;
+  top: ${props => calcTop(props.top)};
   //padding: 1rem;
   background: white;
   //max-height: 700px;
@@ -65,11 +96,16 @@ const SubContainer = styled('div')`
   white-space: nowrap;
   text-overflow: ellipsis;
   overflow: hidden;
-  
-  ${dynamicStyle}  
-`;
+ `;
+//${dynamicStyle}
 
-const Menu = ({categories, onClose,  ...props }) => {
+const SubContainer = (props) => {
+  return <StyledSubContainer {...props} />
+}
+
+const Menu = ({ categories, onClose, ...props }) => {
+
+  const [myColor, setMyColor] = useState('blue');
 
   // console.log(a,b);
 
@@ -82,9 +118,16 @@ const Menu = ({categories, onClose,  ...props }) => {
   const EnterHover = (e) => {
     logEvent(e);
 
+    console.log('Child is: ', e.target.children[0]);
+
+    const node = ReactDOM
+      .findDOMNode();
+      // .getBoundingClientRect(); //outputs <h3> coordinates
+
     // Need to reposition div child based on this
     //How do we find the child
     // How do we change the css for that child using react
+    setColor();
   }
 
   const LeaveHover = (e) => {
@@ -102,6 +145,7 @@ const Menu = ({categories, onClose,  ...props }) => {
 
   const clicky = (e) => {
     logEvent(e);
+    setColor();
   }
 
   const mover = (e) => {
@@ -109,17 +153,42 @@ const Menu = ({categories, onClose,  ...props }) => {
   }
 
 
+  const H1 = styled.h1`
+    font-size: 48px;
+    color: ${props => props.color};
+  `
+
+  const setColor = () => {
+    const num = Math.trunc(Math.random() * 2);
+    setMyColor(num ? 'green' : 'red')
+  }
+
+  const getColor = () => {
+    const num = Math.trunc(Math.random() * 2);
+    return num ? 'green' : 'red';
+  }
+
   return (
+
     <Backdrop>
+      <H1 color={myColor}>Stuff here</H1>
       <Container>
         {_.times(20, () => categories.map(category => (
           <Fragment>
-            <Item key={++key} onClick={clicky} onMouseOver={MouseOver} onMouseOut={MouseOut} onMouseEnter={EnterHover} onMouseLeave={LeaveHover}>{category.id}: {category.name}</Item>
-            <SubContainer>
-            {category.children.map(sub => (
-                <Item key={++key} onMouseOver={MouseOver} onMouseOut={MouseOut} onMouseEnter={EnterHover} onMouseLeave={LeaveHover}>{sub.id}: {sub.name}</Item>
-            ))}
-            </SubContainer>
+            <Item paul={123} key={++key} onClick={clicky} onMouseOver={MouseOver} onMouseOut={MouseOut}
+                  onMouseEnter={EnterHover} onMouseLeave={LeaveHover}>{category.id}: {category.name}
+              <SubContainer visibility={'hidden'} {...props} left={600}>
+                {category.children.map(sub => (
+                  <MySubItem {...props} key={++key} color={'blue'}
+                                     onMouseOver={MouseOver}
+                                     onMouseOut={MouseOut}
+                                     onMouseEnter={EnterHover}
+                                     onMouseLeave={LeaveHover}>
+                    {sub.id}: {sub.name}
+                  </MySubItem>
+                ))}
+              </SubContainer>
+            </Item>
           </Fragment>
         )))}
       </Container>
